512c6512d62cd9e13d6add55dc22add4185bd7eb
jliliflora <jliliflora@wonderpeople.co.kr>
2024-01-18 17:52:10 +0900

    기본 구성 요소와 코드

--- three.js-master/study/01-basic.js
+++ three.js-master/study/01-basic.js
@@ -27,7 +27,12 @@ class App {
 		/*3차원 모델을 설정하는 _setupModel method 호출*/
 		this._setupModel();
 		/*밑줄로 시작하는 filed와 method가 있는데 이렇게 밑줄로 시작하는 이유는 이 App 클래스 내부에서만 사용되는 private filed, private method라는 의미인데 자바스크립트에서는 클래스를 정의할때 private 성격을 부여할 기능이 없음*/
+		/*이렇게 밑줄로 시작함으로써 개발자간의 약속을 정하는 건데 이 밑줄에 정의를 해놨음으로 app클래스 외부에서는 밑줄로 시작하는 filed 또는 method를 호출해서는 안된다 */
 
+		/* 창 크키가 변경되면 발생하는 onesiz 이벤트에 이 클래스의 resize method를 지정하고 있음*/
+		/* resize 이벤트가 필요한 이유 : renderer나 camera는창 크기가 변경 될때마다 그 크기에 맞게 속성값을 재설정 해줘야함 */
+		/* resize 이벤트에 resize metho를 지정할때 bind를 사용해 지정하는 이유 : resize method안에서 this가 가르키는 객체가 아닌 App 클래스의 객체가 되도록 하기 위함 */
+		/* */
 		window.onresize = this.resize.bond(this);
 		this.resize();
 
