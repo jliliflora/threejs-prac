f12e1c78c68250e21024cb14e31c657b20a8fe9e
jliliflora <syon704@gmail.com>
2024-04-17 16:59:25 +0900

    05 Material

--- study/04-material.js
+++ study/04-material.js
@@ -1,4 +1,5 @@
-import * as THREE from '../build/three.module.js'
+import * as THREE from '../build/three.module.js';
+import { OrbitControls } from "../examples/jsm/controls/OrbitControls.js"
 
 class App {
     constructor(){
@@ -16,11 +17,17 @@ class App {
         this._setupCamera();
         this._setupLight();
         this._setupModel();
+        this._setupControls();
 
         window.onresize = this.resize.bind(this);
         this.resize();
 
         requestAnimationFrame(this.render.bind(this));
+    }
+
+    /*마우스를 통해 자유롭게 화면을 조작할 수 있는 함수 */
+    _setupControls() {
+        new OrbitControls(this._camera, this._divContainer);
     }
 
     _setupCamera() {
@@ -32,7 +39,7 @@ class App {
             0.1,
             100
         );
-        camera.position.z = 2;
+        camera.position.z = 7; /*카메라를 조정해서 10000개가 모두 화면에 보이도록 하려면 여기 값을 높여주면 됨 */
         this._camera = camera;
     }
 
@@ -44,8 +51,10 @@ class App {
         this._scene.add(light);
     }
 
+    /*
+    //PointsMaterial을 적용할 10000개의 포인트를 scene에 추가하는 코드!
     _setupModel() {
-        //PointsMaterial을 적용할 10000개의 포인트를 scene에 추가하는 코드!
+        
         
         const vertices = [];
         for(let i = 0; i < 10000; i++) {
@@ -62,14 +71,53 @@ class App {
             new THREE.Float32BufferAttribute(vertices, 3)
         );
 
+        // 원으로 렌더링 시키기
+        const sprite = new THREE.TextureLoader().load(
+            "../examples/textures/sprites/disc.png"
+        );
+
         const material = new THREE.PointsMaterial({
-            color: 0xff0000,
-            size: 5,
-            sizeAttenuation: false 
+            map: sprite,
+            alphaTest: 0.5,
+            color: "#00ffff",
+            size: 0.1,
+            sizeAttenuation: true  //카메라에서 가까운 포인트와 카메라에서 훨씬 더 먼 포인트의 크기가 다르도록 주는 설정값
         });
 
         const points = new THREE.Points(geometry, material);
         this._scene.add(points);
+    }
+    */
+
+    //Line, LineSegments, LineLoop 사용법
+    _setupModel() {
+        const vertices = [
+            -1, 1, 0,
+            1, 1, 0,
+            -1, -1, 0,
+            1, -1, 0,
+        ];
+
+        const geometry = new THREE.BufferGeometry();
+        geometry.setAttribute("position", new THREE.Float32BufferAttribute(vertices, 3));
+        
+        /*
+        //LineBasicMaterial : 선에 대한 색상만 지정하는 코드
+        const material = new THREE.LineBasicMaterial({
+            color: 0xffff00
+        });
+        */
+
+        //LineDashedMaterial : 선의 길이를 참조해서 재질이 적용되므로 선의 길이를 계산해줌
+        const material = new THREE.LineDashedMaterial({
+            color: 0xffff00,
+            dashSize: 0.2,
+            gapSize: 0.1,
+            scale: 4
+        });
+
+        const line = new THREE.LineLoop(geometry, material); //여기 변경해주면됨
+        this._scene.add(line);
     }
 
     resize() {
